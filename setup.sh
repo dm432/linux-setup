a84585531427a1e2f559b27a36f06208638e83dd
Daniel <daniel.maile@uni-ulm.de>
2024-06-03 17:32:07 +0200

    inital commit

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(whoami)" != "root" ] ; then
+  sudo "$0" "$(whoami)" "$@"
+  exit $?
+fi
+
+USRNAME="$1"
+shift
+if [ "$USRNAME" = "" ] ; then
+  echo "Default local user name as argument required"
+  exit 1
+fi
+
+export ANSIBLE_STDOUT_CALLBACK=debug
+
+LOCAL_INVENTORY=""
+if [ -e inventory_local.ini ] ; then
+  LOCAL_INVENTORY="-i inventory_local.ini"
+fi
+
+ansible-playbook -v setup.yaml -i inventory.ini --extra-vars "local_user=$USRNAME"
+
+apt autoremove -y
