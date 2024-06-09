3e91bad1f3231625cec97c42cb0ebe4aba913bc8
Daniel Maile <daniel@daniel-maile.de>
2024-06-10 00:42:08 +0200

    fix permission errors

--- setup.sh
+++ setup.sh
@@ -2,25 +2,10 @@
 
 set -e
 
-if [ "$(whoami)" != "root" ] ; then
-  sudo "$0" "$(whoami)" "$@"
-  exit $?
-fi
-
-USRNAME="$1"
-shift
-if [ "$USRNAME" = "" ] ; then
-  echo "Default local user name as argument required"
-  exit 1
-fi
+USRNAME="$(whoami)"
 
 export ANSIBLE_STDOUT_CALLBACK=debug
 
-LOCAL_INVENTORY=""
-if [ -e inventory_local.ini ] ; then
-  LOCAL_INVENTORY="-i inventory_local.ini"
-fi
+ansible-playbook -v setup.yaml -i inventory_local.ini --ask-become-pass --extra-vars "local_user=$USRNAME"
 
-ansible-playbook -v setup.yaml $LOCAL_INVENTORY --extra-vars "local_user=$USRNAME"
-
-apt autoremove -y
+sudo apt autoremove -y
