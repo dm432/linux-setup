6b86595f21787227d7dd3b9d8ad0d47a072b9f24
Daniel Maile <daniel@daniel-maile.de>
2024-06-13 11:40:26 +0200

    neovim: truncate telescope path

--- roles/common/files/nvim/lua/dm432/lazy/telescope.lua
+++ roles/common/files/nvim/lua/dm432/lazy/telescope.lua
@@ -1,35 +1,41 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+	tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "kwkarlwang/bufjump.nvim",
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"kwkarlwang/bufjump.nvim",
+	},
 
-    config = function()
-        require('telescope').setup({})
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				path_display = {
+					truncate = 2,
+				},
+			},
+		})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>pws", function()
+			local word = vim.fn.expand("<cword>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>pWs", function()
+			local word = vim.fn.expand("<cWORD>")
+			builtin.grep_string({ search = word })
+		end)
+		vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 
-        require("bufjump").setup({
-            forward_key = "<C-n>",
-            backward_key = "<C-m>",
-            on_success = nil
-        })
-    end
+		require("bufjump").setup({
+			forward_key = "<C-n>",
+			backward_key = "<C-m>",
+			on_success = nil,
+		})
+	end,
 }
