29543e4a3e39b00bf4098da9342351cf924a4dac
Daniel Maile <daniel@daniel-maile.de>
2024-10-09 15:49:47 +0200

    neovim: disable vimtex forward search on start

--- roles/common/files/nvim/lua/dm432/lazy/latex.lua
+++ roles/common/files/nvim/lua/dm432/lazy/latex.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         vim.g.vimtex_view_method = 'zathura'
         vim.g.vimtex_compiler_method = 'latexmk'
+        vim.g.vimtex_view_forward_search_on_start = 0
 
         vim.api.nvim_set_keymap('n', '<leader>ltc', '<cmd>VimtexCompile<CR>', { noremap = true, silent = true })
         vim.api.nvim_set_keymap('n', '<leader>ltv', '<cmd>VimtexView<CR>', { noremap = true, silent = true })
