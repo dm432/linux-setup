3eec8edb5f5c21a6359fe5e8419a093b40a0758e
Daniel Maile <daniel@daniel-maile.de>
2025-06-03 09:17:24 +0200

    modify key remaps

--- roles/common/files/utilities/coding-keyboard.sh
+++ roles/common/files/utilities/coding-keyboard.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xmodmap -e "keycode 47 = braceleft bracketleft"
-xmodmap -e "keycode 48 = braceright bracketright"
-xmodmap -e "keycode 34 = backslash"
+xmodmap -e "keycode 47 = braceleft bracketleft braceleft bracketleft odiaeresis Odiaeresis"
+xmodmap -e "keycode 48 = braceright bracketright braceright bracketright adiaeresis Adiaeresis"
+xmodmap -e "keycode 34 = backslash asciitilde backslash asciitilde udiaeresis Udiaeresis"
